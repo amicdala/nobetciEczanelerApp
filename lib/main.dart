add59ee7e51d127015b2f6b6ccdec86085f52e41
unknown <melikhan.hosdogdu@gmail.com>
2021-01-01 20:21:03 +0300

    deneme

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Melikhan',
+      title: 'Melikhan Hosdogdu',
       home: MyHomePage(),
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
